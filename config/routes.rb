b37c8db4ce96415421351eeb342f63d96243c9a6
muaad <muadh24@gmail.com>
2013-12-01 21:26:00 +0300

    Installed and used rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TheSchoolMan::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
